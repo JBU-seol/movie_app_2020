4b7abab2dcc57236dc59bc55d39481e279c68c03
JBU-seol <seolki100480@gmail.com>
2020-03-30 14:05:20 +0900

    Updates

---
+++ precache-manifest.134b4024c8e24241b44c94aedd3dc168.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72eb195458ffd18ce4b0dd6e93816975",
+    "url": "/github.io/movie_app_2020/index.html"
+  },
+  {
+    "revision": "74f1461d4d2020587aeb",
+    "url": "/github.io/movie_app_2020/static/css/main.b7cf6848.chunk.css"
+  },
+  {
+    "revision": "d81438a4d598e83349fe",
+    "url": "/github.io/movie_app_2020/static/js/2.8d5eb2b8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/movie_app_2020/static/js/2.8d5eb2b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74f1461d4d2020587aeb",
+    "url": "/github.io/movie_app_2020/static/js/main.2317e71d.chunk.js"
+  },
+  {
+    "revision": "d672218ef0a208184c34",
+    "url": "/github.io/movie_app_2020/static/js/runtime-main.0f42cbc9.js"
+  }
+]);
